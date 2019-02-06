e81e623d02630b62ceff6cbaf969c61661d04a19
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-06 02:45:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-  first_challenge["Freddy Mercury"][:favorite_icecream_flavors].exclude?("strawberry")
+  first_challenge["Freddy Mercury"][:favorite_icecream_flavors].reject("strawberry")
 
 
   #remember to return your newly altered contacts hash!
